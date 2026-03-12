924f32a94eca92292ad97d23393b7d9c2702a145
RyanThach22592 <rt22592@my.westlake.school.nz>
2026-03-12 21:59:27 +1300

    Set up flask for all webpages, added database functionality for sign ups page

--- App.py
+++ App.py
@@ -27,7 +27,37 @@ def sign_ups():
     """Sign ups webpage"""
     page_title = "Westlake Clubs - Sign Ups"
 
-    return render_template("sign_ups.html", page_title=page_title)
+    conn = get_db_conn()
+    clubs = conn.execute('SELECT * FROM clubs ORDER BY club_name ASC').fetchall()
+    return render_template("sign_ups.html", page_title=page_title, clubs=clubs)
+
+@app.route('/enquiries')
+def enquiries():
+    """Enquiries webpage"""
+    page_title = "Westlake Clubs - Sign Ups"
+
+    return render_template("enquiries.html", page_title=page_title)
+
+@app.route('/timetable')
+def timetable():
+    """Timetable webpage"""
+    page_title = "Westlake Clubs - Timetable"
+
+    return render_template("timetable.html", page_title=page_title)
+
+@app.route('/create_club')
+def create_club():
+    """Create club webpage"""
+    page_title = "Westlake Clubs - Create Club"
+
+    return render_template("create_club.html", page_title=page_title)
+
+@app.route('/review')
+def review():
+    """Review webpage"""
+    page_title = "Westlake Clubs - Review"
+
+    return render_template("review.html", page_title=page_title)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
