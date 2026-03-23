26ed8c833cbc9dbb3613f6f94a3b1ce76bed14f7
Marcus-Hilder <mh22085@my.westlake.school.nz>
2026-03-23 22:42:40 +1300

    fixed mobile times for cal

--- App.py
+++ App.py
@@ -37,23 +37,22 @@ def timetable():
         year = today_dt.year  
     if not month:
         month = today_dt.month
+    cal = calendar.monthcalendar(year, month)
+    yes = 0
     if not week:
         week_count = 0
         today_week = 0
-        # if in curent month then set week to today else defalt to the first week.
-    yes = 0
-    
-    cal = calendar.monthcalendar(year, month)
-    for week in cal:
-            for day in week:
+        for for_week in cal:
+            for day in for_week:
                 if day == today_dt.day and month == today_dt.month:
-                    today_week = week_count
+                    week = week_count
                     yes = True
                     
             week_count += 1
+        # if in curent month then set week to today else defalt to the first week.
    
     #get the cal for the wanted week
-    cal_week = cal[today_week]
+    cal_week = cal[week]
     if yes == True:
         today_index = cal_week.index(today_dt.day)
         today = cal_week[today_index]
@@ -78,13 +77,9 @@ def timetable():
         club_dic[day["id"]]["club_slot"] = day["club_slot"]
         club_dic[day["id"]]["club_name"] = day["club_name"]
         club_dic[day["id"]]["club_description"] = day["club_description"]
-
         
     for i, time in club_dic.items():
         print(i,time["club_slot"])
-    
-
-
 
     return render_template("timetable2.html",page_title=page_title,cal=cal,cal_week=cal_week,month_name=month_name,club_dic=club_dic, today=today)
 
